0781def8e1b4f0ecc5507c7ba00b92c041f9de3e
kevin <a.kevin.schumacher@gmail.com>
2018-01-20 18:43:47 -0500

    logs

--- src/actions.js
+++ src/actions.js
@@ -393,9 +393,9 @@ class Actions {
     getEmojiUsageReport(message) {
         const emojiNameRegex = /:[a-zA-Z0-9]+:/g;
         EmojiRoutes.getEmojiUsageReport(message.guild.id).then(emojisList => {
-            console.log('actions', emojisList);
             const serverEmojiList = message.client.emojis.map(item => item.name);
             // Filter the emoji list by whats currently on the server
+            console.log('mapped', serverEmojiList);
             const reportEmojisList = emojisList.filter(obj => {
                 const parsedName = obj.emoji.match(emojiNameRegex)[0].split(':')[1];
                 return serverEmojiList.indexOf(parsedName) !== -1
