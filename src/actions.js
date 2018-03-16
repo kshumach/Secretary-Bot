613edc3f5a9cf2a9fbdf61d170ea99ca63c1372d
kevin <a.kevin.schumacher@gmail.com>
2018-03-15 22:40:07 -0400

    Add space

--- src/actions.js
+++ src/actions.js
@@ -399,7 +399,7 @@ class Actions {
                 return serverEmojiList.indexOf(parsedName) !== -1
             });
             const reportMessage = reportEmojisList.map(obj => {
-                return `${obj.emoji}: ${obj.usage_count}`
+                return `${obj.emoji} : ${obj.usage_count}`
             }).join(', ');
             message.channel.send(`Emoji Usage: ${reportMessage}`).then(() => {
                 message.channel.send('End Report'); // Need to send some message otherwise the next message cant parse emojis
